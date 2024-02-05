23d8dc6447947381ee36a847aa6800618eefe202
SRE <Nerocross507@gmail.com>
2024-02-05 16:42:30 +0300

    fix def hide_data_payment (счет - done)

--- utils.py
+++ utils.py
@@ -27,16 +27,46 @@ def sorted_date():
 def format_date():
     date_fix = []
     data = sorted_date()
-    for dicts in data:
+    for dict_ in data:
         new_dicts = {}
-        for k, v in dicts.items():
+        for k, v in dict_.items():
             if k == 'date':
                 result = re.sub('T', ' ', v)
                 result = datetime.datetime.strptime(result, '%Y-%m-%d %H:%M:%S.%f').strftime('%d.%m.%Y')
-                dicts[k] = result
-                new_dicts.update(dicts)
+                dict_[k] = result
+                new_dicts.update(dict_)
             else:
-                new_dicts.update(dicts)
+                new_dicts.update(dict_)
         date_fix.append(new_dicts)
     return date_fix
 
+
+def hide_data_payment():
+
+    date_fix = []
+    data = format_date()
+    for dict_ in data:
+        for k, v in dict_.items():
+            #print(k, v)
+            try:
+                if 'Счет' in v:
+                    split_str = v.split(' ')
+                    words = split_str[0:-1]
+                    hided = split_str[-1]
+                    #print(split_str)
+                    hided_digits = '*' * 2 + hided[-4:]
+                    sum_words = ' '.join(words)
+                    result = sum_words + ' ' + hided_digits
+                    dict_[k] = result
+                elif 'Visa' or 'Maestro' or 'MasterCard' in v:
+                    continue
+            except:
+                continue
+
+
+        date_fix.append(dict_)
+    #return date_fix
+    print(date_fix)
+
+hide_data_payment()
+#print(format_date())
